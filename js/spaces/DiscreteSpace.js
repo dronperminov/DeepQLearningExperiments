3ec08ddf887e6c06ef486f5d293ec87af1c74c01
dronperminov <dronperminov@gmail.com>
2022-07-06 10:10:25 +0300

    add probability sample from discrete space

--- js/spaces/DiscreteSpace.js
+++ js/spaces/DiscreteSpace.js
@@ -8,6 +8,21 @@ DiscreteSpace.prototype.Sample = function() {
     return Math.floor(this.start + Math.random() * this.count)
 }
 
+DiscreteSpace.prototype.ProbabilitySample = function(probabilities) {
+    let p = Math.random()
+    let sum = 0
+    let last = this.count - 1
+
+    for (let i = 0; i < last; i++) {
+        sum += probabilities[i]
+
+        if (p < sum)
+            return this.start + i
+    }
+
+    return this.start + last
+}
+
 DiscreteSpace.prototype.GetShape = function() {
     return this.shape
 }
