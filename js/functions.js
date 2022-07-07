0dcff870fe85ba552e92e7e74e14d3cb7e089051
dronperminov <dronperminov@gmail.com>
2022-07-08 01:34:34 +0300

    add mean, std and standartize functions

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,32 @@ function Argmax(values) {
 
     return imax
 }
+
+function Mean(values) {
+    let mean = 0
+
+    for (let value of values)
+        mean += value
+
+    return mean / values.length
+}
+
+function Std(values, mean) {
+    let std = 0
+
+    for (let value of values)
+        std += (value - mean) * (value - mean)
+
+    return Math.sqrt(std / values.length)
+}
+
+function Standartize(values) {
+    let mean = Mean(values)
+    let std = Std(values, mean)
+
+    if (std < 1e-10)
+        return
+
+    for (let i = 0; i < values.length; i++)
+        values[i] = (values[i] - mean) / std
+}
